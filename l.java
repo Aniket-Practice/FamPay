dbabcac111ba734d364f5dfd7342d0d8c1fd73c1
Aniket Practice <waghmareaniket150@gmail.com>
2024-04-03 21:31:38 +0530

    ODD

---
+++ l.java
@@ -0,0 +1,8 @@
+package local;
+
+public class l {
+    public static void main(String[] args) {
+        System.out.println("You can Scan me also.");
+        System.out.println("Hello World");
+    }
+}
